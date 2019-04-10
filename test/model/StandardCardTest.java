b26bd8239a065e1262e718743c7f121ff01e89f4
Shivam Dutt <shiv75035@gmail.com>
2019-04-09 23:52:12 -0500

    StandardCardTest test class: - added allCardsTest() to ensure that StandardCard.ALL_CARDS fulfulls its specification - rewrote test constraint and code for toStringTest()

--- test/model/StandardCardTest.java
+++ test/model/StandardCardTest.java
@@ -7,6 +7,14 @@ import org.junit.jupiter.api.Test;
 import model.StandardCard.Suit;
 import model.StandardCard.Value;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static model.StandardCard.ALL_CARDS;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StandardCardTest {
@@ -19,6 +27,35 @@ class StandardCardTest {
 
     @AfterEach
     void tearDown() {
+    }
+    
+    /* properties of StandardCard.ALL_CARDS:
+     * - contains a card for every possible suit-value combination
+     * - contains no duplicate cards
+     * - contains no null cards
+     * - the cards are in sorted order
+     */
+    @Test
+    void allCardsTest() {
+    	// contains a card for every possible suit-value combination?
+    	for (final Suit suit: Suit.values()) {
+    		for (final Value value: Value.values()) {
+    			final StandardCard card = new StandardCard(suit, value);
+    			assertTrue(StandardCard.ALL_CARDS.contains(card));
+    		}
+    	}
+    	
+    	// contains no duplicate cards?
+    	final Set<StandardCard> set = new HashSet<>(ALL_CARDS);
+    	assertTrue(set.size() == ALL_CARDS.size());
+    	
+    	// contains no null cards?
+    	assertFalse(set.contains(null));
+    	
+    	// the cards are in sorted order?
+    	final List<StandardCard> list = new ArrayList<>(ALL_CARDS);
+    	Collections.sort(list);
+    	assertEquals(list, ALL_CARDS);
     }
 
     @Test
@@ -42,6 +79,14 @@ class StandardCardTest {
     // every card in ALL_CARDS should return a different value for toString()
     @Test
     void toStringTest() {
-    	assertEquals(card.toString(), "A"+"C"/*'\u2663'*/);
+    	for (final StandardCard card: ALL_CARDS) {
+    		for (final StandardCard card2: ALL_CARDS) {
+        		if (card.equals(card2)) {
+        			assertTrue(card.toString().equals(card2.toString()));
+        		} else {
+        			assertFalse(card.toString().equals(card2.toString()));
+        		}
+        	}
+    	}
     }
 }
