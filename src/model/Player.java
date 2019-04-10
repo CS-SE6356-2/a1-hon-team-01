566f25a8661239be9edb8b6e9807c14cab380b7d
thelandsquid <graweben0@gmail.com>
2019-04-09 19:14:20 -0500

    Changed Player.java to contain Array of ArrayList of Cards
    Player previously contained the now deleted Deck class; we changed it so that the Cards are implemented within separate ArrayLists contained within an array

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,16 +1,18 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  * Represents a player in a card game, can be extended for additional functionality.
  */
 public class Player {
-	private Deck[] decks;
+	private ArrayList[] decks;
 	
 	public Player(final int deckCount) {
-		decks = new Deck[deckCount];
+		decks = new ArrayList[deckCount];
 	}
 	
-	public Deck getDeck(final int index) {
+	public ArrayList getDeck(final int index) {
 		return decks[index];
 	}
 }
